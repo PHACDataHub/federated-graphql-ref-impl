f38176ee66c8e214b76eed70c221b817ca5ac965
Collin Brown <collin.j.brown95@gmail.com>
2023-08-30 16:34:00 -0400

    feat(index.js): add context function to get JWT claims from authz header

--- api-1/index.js
+++ api-1/index.js
@@ -1,8 +1,26 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import jwt from "jsonwebtoken";
 
 import { typeDefs } from './schema.js';
 import { resolvers } from './resolvers.js';
+// Environment variables
+const JWT_VERIFY_SECRET = process.env.JWT_VERIFY_SECRET || "changeme"
+
+// TODO: move getClaims to separate module
+function getClaims(req) {
+    let claims = null;
+    // Header names in Express are auto-converted to lowercase
+    let token = req.headers['x-access-token'] || req.headers['authorization'];
+
+    // Remove Bearer from string
+    token = token.replace(/^Bearer\s+/, "");
+
+    if (token) {
+        claims = jwt.verify(token, JWT_VERIFY_SECRET);
+    }
+    return claims;
+}
 
 // server setup
 const server = new ApolloServer({
@@ -13,7 +31,11 @@ const server = new ApolloServer({
 const { url } = await startStandaloneServer(server, {
     listen: {
         port: 4000
-    }
+    },
+    // Context function should be async and return an object
+    context: async ({ req }) => ({
+        claims: getClaims(req),
+    }),
 });
 
 console.log('Server ready at port', 4000);
