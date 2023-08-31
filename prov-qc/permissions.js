1dd38fba91e3e642771e45389074c5fffd0c2988
Collin Brown <collin.j.brown95@gmail.com>
2023-08-31 09:30:00 -0400

    feat(permissions.js): include example permissions - manager has access to authors, and employee doesn't

--- prov-qc/permissions.js
+++ prov-qc/permissions.js
@@ -1,12 +1,50 @@
-import { shield, rule, deny } from "graphql-shield";
+import { shield, rule, and } from "graphql-shield";
 
 const isAuthenticated = rule()((parent, args, context) => {
-    return context.claims !== undefined;
+    if (context.claims) {
+        return context.claims !== undefined;
+    }
+    return false;
+});
+
+const isManager = rule()((parent, args, context) => {
+    if (context.claims) {
+        return context.claims.role === "Manager";
+    }
+    return false;
 });
 
 // TODO: Add default deny rule
 export default shield({
+    Game: {
+        id: isAuthenticated,
+        title: isAuthenticated,
+        platform: isAuthenticated,
+        reviews: isAuthenticated,
+    },
+    Review: {
+        id: isAuthenticated,
+        rating: isAuthenticated,
+        content: isAuthenticated,
+        game: isAuthenticated,
+        author: and(isAuthenticated, isManager),
+    },
+    Author: {
+        id: isAuthenticated,
+        name: isAuthenticated,
+        verified: isAuthenticated,
+        reviews: isAuthenticated,
+    },
     Query: {
+        reviews: isAuthenticated,
+        review: isAuthenticated,
         games: isAuthenticated,
-    }
+        game: isAuthenticated,
+        authors: and(isAuthenticated, isManager),
+        author: and(isAuthenticated, isManager),
+    },
+    Mutation: {
+        deleteGame: isAuthenticated,
+        addGame: isAuthenticated,
+    },
 });    
